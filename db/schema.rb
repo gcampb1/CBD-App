6860059e5953520bc514a6639581ad90d2ba287a
Garrett D. Campbell <gcampb1@gmail.com>
2017-10-18 21:26:36 -0500

    Setup carrierwave

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019011833) do
+ActiveRecord::Schema.define(version: 20171019015155) do
 
   create_table "tweets", force: :cascade do |t|
     t.string "message"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20171019011833) do
     t.string "username"
     t.string "location"
     t.text "bio"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
