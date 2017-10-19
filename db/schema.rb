02dd6eb992d468113c222f4a6cd89d3e710c137c
Garrett D. Campbell <gcampb1@gmail.com>
2017-10-18 20:01:08 -0500

    Whitelist new profile fields and add them to the forms

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019002156) do
+ActiveRecord::Schema.define(version: 20171019003914) do
 
-  create_table "u_sers", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -25,8 +25,12 @@ ActiveRecord::Schema.define(version: 20171019002156) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_u_sers_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_u_sers_on_reset_password_token", unique: true
+    t.string "name"
+    t.string "username"
+    t.string "location"
+    t.text "bio"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
